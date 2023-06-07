bc8262828a421bede215e0206d26a02f47f568ce
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2023-06-07 22:39:27 +0000

    starting to learn C#

--- main.cs
+++ main.cs
@@ -3,7 +3,11 @@ using System;
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
+
+    // string
     String name = "Mohameth";
+
+    //integer
     int age = 26;
 
     Console.WriteLine(name + " is " + age);
