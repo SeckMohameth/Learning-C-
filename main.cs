445c05830aa05d5c9aacc7c9b8d50c989fb3f786
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2023-06-07 19:35:23 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    String name = "Mohameth";
+    int age = 26;
+
+    Console.WriteLine(name + " is " + age);
+
+  }
+  
+}
+
