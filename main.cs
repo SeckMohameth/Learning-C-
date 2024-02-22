c6bf61a788fc9ea213f2abf7b51e56c0b93d3260
SeckMohameth <30297403+SeckMohameth@users.noreply.github.com>
2024-02-22 03:52:11 +0000

    worked the basics on another editor

--- main.cs
+++ main.cs
@@ -8,12 +8,32 @@ class Program {
     /*  
       multiline comment
     */
-    
-    Console.WriteLine ("Hello World");
 
-    
+    int mo = 27;
+    if (mo > 26) {
+      Console.WriteLine("is 27");
+    } else {
+      Console.WriteLine("Younger than 27");
+    }
 
+    int i = 44;
+    
+    Console.WriteLine ("Hello World" + i);
+
+    testfunction();
+    secondFunction(23);
+    
   }
+
+  static void testfunction() {
+    Console.WriteLine("I'm another function");
+  }
+
+  static bool secondFunction(int i) {
+    return  i < 100;
+  }
+
+  
   
 }
 
